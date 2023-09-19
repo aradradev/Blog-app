3f059a1f692949a82165a273958f5766725bc284
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-19 21:03:15 +0000

    generating new models

--- db/migrate/20230919204451_create_users.rb
+++ db/migrate/20230919204451_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :photo_url
+      t.string :photo
+      t.text :bio
+      t.integer :posts_counter
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_144536) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_205816) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
     t.integer "author_id"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "photo_url"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
