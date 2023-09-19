9fba6d181e26b63daec1264ed8519603a12fcb95
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-19 16:07:40 +0000

    add data migration for users and posts

---
+++ db/migrate/20230919144536_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
