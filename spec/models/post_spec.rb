58106310d422300575d25fc5ae13972a9e4e3805
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-21 16:26:28 +0000

    Fix linter error

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Post, type: :model do
-  let(:user) { User.new(name: 'Tom', posts_counter: 0)}
+  let(:user) { User.new(name: 'Tom', posts_counter: 0) }
   it 'is valid with title, comments_counter and likes_counter greater than or equal to 0' do
     post = Post.new(author: user, title: 'Sample Title', comments_counter: 0, likes_counter: 0)
     expect(post).to be_valid
@@ -23,12 +23,12 @@ RSpec.describe Post, type: :model do
   end
 
   it 'is valid if the title is under 250 characters' do
-    title1='a'*39;
+    title1 = 'a' * 39
     post = Post.new(author: user, title: title1, comments_counter: 0, likes_counter: 0)
     expect(post).to be_valid
   end
   it 'is invalid if the title exceeds 250 characters' do
-    title2='a'*251;
+    title2 = 'a' * 251
     post = Post.new(author: user, title: title2, comments_counter: 0, likes_counter: 0)
     expect(post).to_not be_valid
   end
