86fd2c294572e447b876240b6c7182295c7f7f91
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-21 22:42:12 +0000

    updating the like class for the test

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,14 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates :user_id, presence: true, numericality: { only_integer: true }
+
+  validates :post_id, presence: true, numericality: { only_integer: true }
+  before_create :update_post_likes_counter
+
+  private
+
   def update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
+    post.update(likes_counter: post.likes.count + 1)
   end
 end
