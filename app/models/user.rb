1fc5a5e218ffc3c30b3607e36b9c18caada7a4db
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-21 22:42:46 +0000

    updating and adapting the new test case

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+  before_create :recent_posts
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
