22c0d2a7c13cf08b509003b2227eed86f0b4be7c
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-23 21:08:34 +0000

    add some config in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+  get 'users/:id' => 'users#show', as: :user
+  get 'posts/:id' => 'posts#show', as: :post
 end
