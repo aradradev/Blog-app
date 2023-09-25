ac8faa73555573d7e090551eb0cb1b41ac379bfc
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-25 16:15:12 +0000

    Fix issue required by the reviewer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get 'users/:id' => 'users#show', as: :user
-  get 'posts/:id' => 'posts#show', as: :post
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show]
 end
