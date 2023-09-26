c4a178531ce25a1724d974a356ab249f0a34dd31
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-26 13:16:00 +0000

    adding schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_205816) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_132713) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
