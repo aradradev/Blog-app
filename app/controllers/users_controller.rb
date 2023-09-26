9689a49bb11b81e551676a384a8596465927e970
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-26 13:15:21 +0000

    adding users controller index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.includes(:posts)
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
