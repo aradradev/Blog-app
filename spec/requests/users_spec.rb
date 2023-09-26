d957368e993e145356211eb92c155d28e50eed82
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-26 18:31:57 +0000

    adding users spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,29 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-  describe 'GET /index' do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
     it 'responds with 200 OK' do
-      get :index
+      get '/users'
       expect(response).to have_http_status(:ok)
     end
 
-    it 'renders the index template' do
-      get :index
+    it 'should render the index template Users correctly' do
+      get '/users'
       expect(response).to render_template(:index)
+    end
+    
+    it 'should include the correct placeholder text index' do
+      get '/users'
+      expect(response.body).to include('Welcome to my Blog app!')
+      expect(response.body).to include('User1: John Doe.')
     end
   end
 
-  describe 'Get /show' do
-    it 'responds show with 200 OK' do
-      user = User.create(name: 'Jalloh', posts_counter: 0)
-      get :show, params: { id: user.id }
+  describe 'GET /users:id' do
+    it 'should responds with 200 OK' do
+      user = User.create(name: 'Tommy', posts_counter: 0)
+      get "/users/#{user.id}"
       expect(response).to have_http_status(:ok)
     end
-
-    it 'it renders the show template' do
-      user = User.create(name: 'John', posts_counter: 0)
-      get :show, params: { id: user.id }
+    it 'should renders correct placeholder text show' do
+      user = User.create(name: 'John')
+      get "/users/#{user.id}"
       expect(response).to render_template(:show)
+    end
+
+    it 'should includes correct information and render properly from the show' do
+      user = User.create(name: 'Jalloh', posts_counter: 0)
+      get "/users/#{user.id}"
+      expect(response.body).to include('Jalloh')
+      expect(response.body).to include('Posts: 0')
     end
   end
 end
