67dd7585dac1f2b9cbf1758416ade6fed36f1e04
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-26 13:17:21 +0000

    adding user controller spec in the reuests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe UsersController, type: :controller do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'responds with 200 OK' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'Get /show' do
+    it 'responds show with 200 OK' do
+      user = User.create(name: 'Jalloh', posts_counter: 0)
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'it renders the show template' do
+      user = User.create(name: 'John', posts_counter: 0)
+      get :show, params: { id: user.id }
+      expect(response).to render_template(:show)
+    end
   end
 end
