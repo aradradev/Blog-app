1aef2d0d430625e9ea64fb0e2be96d470c20132e
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-28 18:41:03 +0000

    config routes for the new post and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create]
+  end
 end
