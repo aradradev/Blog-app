79e9aad1cea695522ec61ae93e7c3b117e0a44b4
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-28 18:38:11 +0000

    adding posts controller create and new method to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
