0a6b1e62ae3151fd59fb3a4e574b9da50d1cd38f
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-28 21:45:49 +0000

    Fixing handlers submit button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    comment_text = comment_params[:text].strip
-    if comment_text.present?
-      @comment = current_user.comments.build(comment_params.merge(post_id: @post.id))
-
-      if @comment.save
-        flash[:notice] = 'Comment was successfully created.'
-        redirect_to @post
-      else
-        flash[:alert] = 'Comment could not be saved.'
-        render '/posts/show'
-      end
+    @comment = current_user.comments.build(comment_params.merge(post_id: @post.id))
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created.'
+      redirect_to @post
     else
+      @comments = @post.comments
       flash[:alert] = 'Comment cannot be empty.'
       render '/posts/show'
     end
