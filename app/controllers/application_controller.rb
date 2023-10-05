2e38710406bc9831712ec91db3e08cb5f59e70e5
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-05 11:17:31 +0000

    adding the application controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,29 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_confirmation_path_for(resource)
+    user_path(resource)
+  end
+
+
+
   protected
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :current_password, :password_confirmation, :bio)
+      u.permit(:name, :email, :password, :current_password, :password_confirmation, :bio, :photo)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :current_password, :password_confirmation, :bio)
+      u.permit(:name, :email, :password, :current_password, :password_confirmation, :bio, :photo)
     end
   end
 end
