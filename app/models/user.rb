a5a7a8d30b114bed96c9742e1b357087261cbf79
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-05 11:18:34 +0000

    handle the case when a user is deleted all posts should be deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments
   has_many :likes
+  has_one_attached :photo
   before_create :recent_posts
+
+  before_destroy :delete_user_posts
+
+  def delete_user_posts
+    posts.destroy_all
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
