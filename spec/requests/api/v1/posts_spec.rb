0bbe4bf643af0666eb9a82ae713638ea02e4bf54
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-08 00:36:38 +0000

    implementing all api endpoints and testing postman

---
+++ spec/requests/api/v1/posts_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/v1/posts/index'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
